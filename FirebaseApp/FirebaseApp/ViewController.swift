ede1fa9f7a1ace17e3eebd4095058ee2664f5115
Sofa <sofasven00@gmail.com>
2023-11-11 17:54:50 +0300

    Project created

---
+++ FirebaseApp/FirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Sofa on 11.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
